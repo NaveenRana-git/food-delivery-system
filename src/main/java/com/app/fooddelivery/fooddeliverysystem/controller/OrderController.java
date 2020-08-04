44c3466b5e7a7b0472ea68f45b0e75e85788d84f
NaveenRana-git <69208422+NaveenRana-git@users.noreply.github.com>
2020-08-04 23:05:20 +0530

    changes commit

--- src/main/java/com/app/fooddelivery/fooddeliverysystem/controller/OrderController.java
+++ src/main/java/com/app/fooddelivery/fooddeliverysystem/controller/OrderController.java
@@ -1,7 +1,8 @@
 package com.app.fooddelivery.fooddeliverysystem.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +20,8 @@ public class OrderController {
 		dao = theOrderDao;
 	}
 	
-	@GetMapping("/Orders")
-	public Orders saveOrder(Orders od) {
+	@PostMapping("/orders")
+	public Orders saveOrder(@RequestBody Orders od) {
 		return dao.saveOrder(od);
 		
 	}
